649b3b3f1757c9349dd032cd3bf44925e530ad69
yuvi7-star <yuvrajsarvaiya8@gmail.com>
2020-03-30 21:36:55 +0530

    added threenumsum file

---
+++ ThreeNumSum.java
@@ -0,0 +1,16 @@
+
+class TestThreeNumSum {
+	 static int threeNumSum(int firstNumber, int seconNumber, int thirdNumber) {
+	 	int addition = firstNumber + seconNumber + thirdNumber;
+	 	return addition;
+	 }
+}
+
+
+
+class ThreeNumSum {
+	public static void main(String[] args) {
+		int addition = TestThreeNumSum.threeNumSum(20,30,50);
+		System.out.println("Addtion = "+addition);
+	}
+}
