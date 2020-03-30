cbcd4266d04f8f0499b8359c14d9c82f4bad21bd
yuvi7-star <yuvrajsarvaiya8@gmail.com>
2020-03-30 22:12:46 +0530

    changed classname

--- ThreeNumSum.java
+++ ThreeNumSum.java
@@ -1,16 +1,14 @@
 
-class TestThreeNumSum {
+class TestThreeSum {
 	 static int threeNumSum(int firstNumber, int seconNumber, int thirdNumber) {
 	 	int addition = firstNumber + seconNumber + thirdNumber;
 	 	return addition;
 	 }
 }
 
-
-
 class ThreeNumSum {
 	public static void main(String[] args) {
-		int addition = TestThreeNumSum.threeNumSum(20,30,50);
+		int addition = TestThreeSum.threeNumSum(20,30,50);
 		System.out.println("Addtion = "+addition);
 	}
 }
